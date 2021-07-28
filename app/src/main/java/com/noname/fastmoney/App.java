2d64019be4bfbe46bd2f041084adc45ef278b1a7
Maxim Tolstobrov <tmaksim1989@gmail.com>
2021-07-28 15:06:24 +0300

    Create OneSignal

--- app/src/main/java/com/noname/fastmoney/App.java
+++ app/src/main/java/com/noname/fastmoney/App.java
@@ -6,16 +6,26 @@ import androidx.room.Room;
 
 import com.noname.fastmoney.data.database.AppDatabase;
 import com.noname.fastmoney.data.entities.User;
+import com.onesignal.OneSignal;
 
 public class App extends Application {
 
     public static App instance;
+    private static final String ONESIGNAL_APP_ID = "7a9d5638-813f-4ed6-8cbe-5a0f44b91182";
 
     private AppDatabase mDatabase;
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
+
         instance = this;
         mDatabase = Room.databaseBuilder(this, AppDatabase.class, "database")
                 .allowMainThreadQueries()
